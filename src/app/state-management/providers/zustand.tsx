51cd27e5b3f8b6e43e4f6a924c1d7b177f714c6a
markshawn2020 <shawninjuly@gmail.com>
2024-03-10 22:06:53 +0800

    implemented zustand derive method

--- src/app/state-management/providers/zustand.tsx
+++ src/app/state-management/providers/zustand.tsx
@@ -4,20 +4,59 @@ import { create } from "zustand";
 import { person, Person } from "../schema";
 import { Shared } from "../shared";
 
-export const usePersonStoreZustand = create<Person>(
+export const usePersonClassZustand = create<Person>(
   (set, get, store) => person,
 );
 
+interface IPerson {
+  firstName: string;
+  secondName: string;
+  computed: {
+    name: string;
+  };
+  updateSecondName: (secondName: string) => void;
+}
+
+export const usePersonStandardZustand = create<IPerson>((set, get) => ({
+  firstName: person.firstName,
+  secondName: person.secondName,
+
+  // ref: https://github.com/pmndrs/zustand/issues/132#issuecomment-1120467721
+  computed: {
+    get name() {
+      return get().firstName + " " + get().secondName;
+    },
+  },
+  updateSecondName(secondName: string) {
+    set((state) => ({
+      secondName,
+    }));
+  },
+}));
+
 export const ZustandPerson = () => {
-  const person = usePersonStoreZustand();
+  const person = usePersonClassZustand();
+  const {
+    computed: { name },
+    updateSecondName,
+  } = usePersonStandardZustand();
 
   return (
-    <Shared
-      title={"zustand (no action)"}
-      name={person.name}
-      updateSecondName={(value) => {
-        person.updateSecondName(value);
-      }}
-    />
+    <>
+      <Shared
+        title={"zustand (no action)"}
+        name={person.name}
+        updateSecondName={(value) => {
+          person.updateSecondName(value);
+        }}
+      />
+
+      <Shared
+        title={"zustand (computed)"}
+        name={name}
+        updateSecondName={updateSecondName}
+        ok
+      />
+    </>
   );
 };
